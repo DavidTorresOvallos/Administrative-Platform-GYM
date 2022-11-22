09689fa9401c248fc7cf05b0415fc40998b2b465
DavidTorresOvallos <davidtorresovallos@gmail.com>
2022-11-22 11:02:05 -0500

    Login Translucido y ms bonito

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   end
 
   get 'session/index'
-  root 'session#index'
+  root to: 'session#index'
 #  devise_for :employees
   get 'dashboard/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
